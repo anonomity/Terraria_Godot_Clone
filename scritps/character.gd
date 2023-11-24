ad399c71fdbbee84f83ad7e817c442feafc33dd6
Jackie <jlauravelez@gmail.com>
2023-11-24 18:17:27 +0100

    wizard and digging

--- scritps/character.gd
+++ scritps/character.gd
@@ -7,11 +7,13 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player_2 = $AnimationPlayer2
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,9 +22,26 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction >= 0:
+		sprite_2d.flip_h = true
+	else:
+		sprite_2d.flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
+		animation_player.play("run")
 	else:
+		animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("click"):
+		animation_player_2.play("chop")
+	if Input.is_action_just_released("click"):
+		animation_player_2.play("idle")
 
+	# Add the gravity.
+	if not is_on_floor():
+		animation_player.play("jump")
+		velocity.y += gravity * delta
 	move_and_slide()
--- scritps/world_gen/world_gen.gd
+++ scritps/world_gen/world_gen.gd
@@ -12,34 +12,33 @@ var dirt_grass_atlas = Vector2(0,0)
 var dirt_atlas = Vector2(1,0)
 var left_ramp = Vector2(2,0)
 var right_ramp = Vector2(3,0)
-
-var height_tile_arr = []
-var dirt_fill_arr = []
-
+var tile_arr = []
 func _ready():
 	var noise : FastNoiseLite = noise_image.noise
 
-	for x in width:
+	for x in (width):
 		
 		
 		var noise_height = int(noise.get_noise_1d(x) * 10)
 		
-
-	
 		# filling in the first
 		for y in height:
-			dirt_fill_arr.append(Vector2(x, noise_height+y))
+			tile_arr.append(Vector2(x, noise_height+y))
 			
 			
+		tile_arr.append(Vector2(x, noise_height))
 
-	
-		height_tile_arr.append(Vector2(x, noise_height))
-	
 
-	#filling the height
-	BetterTerrain.set_cells(tile_map, 0,height_tile_arr, 0)
-	BetterTerrain.update_terrain_cells(tile_map, 0,height_tile_arr,true )
-	
-	# filling in the dirt
-	BetterTerrain.set_cells(tile_map, 0,dirt_fill_arr, 0)
-	BetterTerrain.update_terrain_cells(tile_map, 0,dirt_fill_arr,true )
+
+	BetterTerrain.set_cells(tile_map, 0,tile_arr, 0)
+	BetterTerrain.update_terrain_cells(tile_map, 0,tile_arr,true )
+
+func _input(event):
+	if Input.is_action_just_pressed("click"):
+		var mouse_pos = get_global_mouse_position()
+		var tile_pos = tile_map.local_to_map(mouse_pos)
+		var index = BetterTerrain.get_cell(tile_map, 0, tile_pos)
+		if index != -1:
+			tile_map.erase_cell(0, tile_pos)
+			#tile_arr.erase(tile_pos)
+			BetterTerrain.update_terrain_cell(tile_map, 0,tile_pos,true )
