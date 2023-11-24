ff513d4811ee67b77896b29b60f3cda2271d0c25
Jackie <jlauravelez@gmail.com>
2023-11-24 16:39:58 +0100

    ramps and better terrain

--- scritps/world_gen/world_gen.gd
+++ scritps/world_gen/world_gen.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var tile_map = $TileMap
 
 
-var width : int = 10000
+var width : int = 1000
 var height: int = 100
 
 var dirt_grass_atlas = Vector2(0,0)
@@ -13,25 +13,33 @@ var dirt_atlas = Vector2(1,0)
 var left_ramp = Vector2(2,0)
 var right_ramp = Vector2(3,0)
 
+var height_tile_arr = []
+var dirt_fill_arr = []
+
 func _ready():
 	var noise : FastNoiseLite = noise_image.noise
-	var past_height = null
+
 	for x in width:
 		
 		
 		var noise_height = int(noise.get_noise_1d(x) * 10)
 		
-		#
-		#if (past_height != noise_height) and past_height != null:
-			#if past_height > noise_height:
-				#tile_map.set_cell(0, Vector2(x-1, noise_height), 0,right_ramp)
-			#elif past_height < noise_height:
-				#tile_map.set_cell(0, Vector2(x+1, noise_height), 0,left_ramp)
-		past_height = noise_height
+
+	
 		# filling in the first
 		for y in height:
-			tile_map.set_cell(0, Vector2(x, noise_height+y), 0,dirt_atlas)
+			dirt_fill_arr.append(Vector2(x, noise_height+y))
 			
-		tile_map.set_cell(0, Vector2(x, noise_height), 0,dirt_grass_atlas)
+			
+
+	
+		height_tile_arr.append(Vector2(x, noise_height))
 	
 
+	#filling the height
+	BetterTerrain.set_cells(tile_map, 0,height_tile_arr, 0)
+	BetterTerrain.update_terrain_cells(tile_map, 0,height_tile_arr,true )
+	
+	# filling in the dirt
+	BetterTerrain.set_cells(tile_map, 0,dirt_fill_arr, 0)
+	BetterTerrain.update_terrain_cells(tile_map, 0,dirt_fill_arr,true )
