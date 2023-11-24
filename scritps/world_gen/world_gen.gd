294630a65d1e95934e03d32eeb799478c571f058
Jackie <jlauravelez@gmail.com>
2023-11-24 19:00:37 +0100

    prettier saves

--- scritps/world_gen/world_gen.gd
+++ scritps/world_gen/world_gen.gd
@@ -19,6 +19,8 @@ var tile_arr = []
 
 var black_tile = Vector2(4,0)
 var white_tile = Vector2(5,0)
+var front_layer = 1
+var back_layer = 0
 
 func _ready():
 	var noise : FastNoiseLite = noise_image.noise
@@ -32,30 +34,32 @@ func _ready():
 		# filling in the first
 		for y in height:
 			
-			
+			if y > 5:
+				tile_map.set_cell(back_layer,Vector2(x, noise_height+y), 0,Vector2(6, 0))
 			#if y > 50:
 				#print(cave_noise.get_noise_2d(x,y))
 			if cave_noise.get_noise_2d(x,y) < 0.4:
 				tile_arr.append(Vector2(x, noise_height+y))
 			
 			
-		tile_arr.append(Vector2(x, noise_height))
+		if tile_arr.find(Vector2(x, noise_height+1)) != -1:
+			tile_arr.append(Vector2(x, noise_height))
 
 
 
-	BetterTerrain.set_cells(tile_map, 0,tile_arr, 0)
-	BetterTerrain.update_terrain_cells(tile_map, 0,tile_arr,true )
+	BetterTerrain.set_cells(tile_map, front_layer,tile_arr, 0)
+	BetterTerrain.update_terrain_cells(tile_map, front_layer,tile_arr,true )
 
 func _input(event):
 	if Input.is_action_just_pressed("click"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_pos = tile_map.local_to_map(mouse_pos)
-		var index = BetterTerrain.get_cell(tile_map, 0, tile_pos)
+		var index = BetterTerrain.get_cell(tile_map, front_layer, tile_pos)
 		if index != -1:
-			tile_map.erase_cell(0, tile_pos)
+			tile_map.erase_cell(front_layer, tile_pos)
 			#tile_arr.erase(tile_pos)
-			BetterTerrain.update_terrain_cell(tile_map, 0,tile_pos,true )
+			BetterTerrain.update_terrain_cell(tile_map, front_layer,tile_pos,true )
 	if Input.is_action_just_pressed("right_click"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_pos = tile_map.local_to_map(mouse_pos)
-		tile_map.set_cell(0, tile_pos, 0,dirt_atlas)
+		tile_map.set_cell(front_layer, tile_pos, 0,dirt_atlas)
